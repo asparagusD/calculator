25e0c47d61ebe7efb84bae26793474ff228960e5
asparagusD <nufel1130@gmail.com>
2025-02-04 02:51:50 +0600

    Finish design the UI
    Created necessary buttons of the calculator and colored them according to their specific functionality

--- bin.js
+++ bin.js
@@ -1,3 +1,31 @@
+const bigDiv = document.querySelector("#display");
+let div = [];
+let buttonText = [
+    ["AC", "Delete", "%", "/"],
+    ["7", "8", "9", "*"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", "", ".", "="],
+];
+
+for (let i=0; i<5; i++) {
+    div[i] = document.createElement("div");
+    div[i].setAttribute("id", `row${i}`);
+    div[i].style.cssText = "flex: 1; display: flex; background-color: navy;";
+    bigDiv.appendChild(div[i]);
+
+    for (let j=0; j<4; j++) {
+        if (i===4 && j===1) continue;
+        const btn = document.createElement("button");
+        btn.textContent = buttonText[i][j];
+        btn.setAttribute("id", `column${j}`);
+        div[i].appendChild(btn);
+        
+    }
+}
+
+
+
 let number1;
 let number2;
 let operator;
