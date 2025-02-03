4449b2ec8a9be05e123344913c72904c25911821
asparagusD <nufel1130@gmail.com>
2025-02-03 21:15:55 +0600

    Create basic functionality of the calculator
    Three variables are created for two numbers and one operator. Four functions are created for calculations and another function for choosing the operator

---
+++ bin.js
@@ -0,0 +1,36 @@
+let number1;
+let number2;
+let operator;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return  a / b;
+}
+
+function operate(number1, number2, operator) {
+    switch(operator) {
+        case '+':
+            add(number1, number2);
+            break;
+        case '-':
+            subtract(number1, number2);
+            break;
+        case '*':
+            multiply(number1, number2);
+            break;
+        case '/':
+            divide(number1, number2);
+            break;            
+    }
+}
