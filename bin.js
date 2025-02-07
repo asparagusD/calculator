c1d85331f508e802c43faf736328d72e1298949a
asparagusD <nufel1130@gmail.com>
2025-02-08 00:11:19 +0600

    Create functionality for all the orange buttons
    Built the logic for addition (+), subtraction (-), multiplication (*), division (/) and equals to (=) buttons

--- bin.js
+++ bin.js
@@ -1,13 +1,14 @@
-let number1;
-let number2;
-let operator;
-let num;
+let number;
+let op = 1;
+let operator = [];
+operator[0] = "+";
+let sum = 0;
 let result;
-
 
 const bigDiv = document.querySelector("#display");
 let disp = document.createElement("textarea");
-disp.textContent = 0;
+result = 0;
+disp.textContent = result;
 bigDiv.appendChild(disp);
 let div = [];
 let button = [];
@@ -39,8 +40,10 @@ for (let i=0; i<5; i++) {
 button[0] = document.querySelector(`#row1 #column0`);    //7
 button[0].addEventListener("click", (event) => {
     if (disp.textContent.length < 11) {
-        disp.textContent = disp.textContent * 10 + +button[0].textContent;
-        num = disp.textContent;
+        result = result * 10 + +button[0].textContent;
+        disp.textContent = result;
+        number = result;
+        
     } else {
         event.preventDefault();
     }    
@@ -49,8 +52,10 @@ button[0].addEventListener("click", (event) => {
 button[1] = document.querySelector(`#row1 #column1`);    //8
 button[1].addEventListener("click", (event) => {
     if (disp.textContent.length < 11) {
-        disp.textContent = disp.textContent * 10 + +button[1].textContent;
-        num = disp.textContent;
+        result = result * 10 + +button[1].textContent;
+        disp.textContent = result;
+        number = result;
+        
     } else {
         event.preventDefault();
     } 
@@ -59,8 +64,9 @@ button[1].addEventListener("click", (event) => {
 button[2] = document.querySelector(`#row1 #column2`);    //9
 button[2].addEventListener("click", (event) => {
     if (disp.textContent.length < 11) {
-        disp.textContent = disp.textContent * 10 + +button[2].textContent;
-        num = disp.textContent;
+        result = result * 10 + +button[2].textContent;
+        disp.textContent = result;
+        number = result;
     } else {
         event.preventDefault();
     } 
@@ -69,8 +75,9 @@ button[2].addEventListener("click", (event) => {
 button[3] = document.querySelector(`#row2 #column0`);    //4
 button[3].addEventListener("click", (event) => {
     if (disp.textContent.length < 11) {
-        disp.textContent = disp.textContent * 10 + +button[3].textContent;
-        num = disp.textContent;
+        result = result * 10 + +button[3].textContent;
+        disp.textContent = result;
+        number = result;
     } else {
         event.preventDefault();
     } 
@@ -79,8 +86,9 @@ button[3].addEventListener("click", (event) => {
 button[4] = document.querySelector(`#row2 #column1`);    //5
 button[4].addEventListener("click", (event) => {
     if (disp.textContent.length < 11) {
-        disp.textContent = disp.textContent * 10 + +button[4].textContent;
-        num = disp.textContent;
+        result = result * 10 + +button[4].textContent;
+        disp.textContent = result;
+        number = result;
     } else {
         event.preventDefault();
     } 
@@ -89,8 +97,9 @@ button[4].addEventListener("click", (event) => {
 button[5] = document.querySelector(`#row2 #column2`);    //6
 button[5].addEventListener("click", (event) => {
     if (disp.textContent.length < 11) {
-        disp.textContent = disp.textContent * 10 + +button[5].textContent;
-        num = disp.textContent;
+        result = result * 10 + +button[5].textContent;
+        disp.textContent = result;
+        number = result;
     } else {
         event.preventDefault();
     } 
@@ -99,8 +108,9 @@ button[5].addEventListener("click", (event) => {
 button[6] = document.querySelector(`#row3 #column0`);    //1
 button[6].addEventListener("click", (event) => {
     if (disp.textContent.length < 11) {
-        disp.textContent = disp.textContent * 10 + +button[6].textContent;
-        num = disp.textContent;
+        result = result * 10 + +button[6].textContent;
+        disp.textContent = result;
+        number = result;
     } else {
         event.preventDefault();
     } 
@@ -109,8 +119,9 @@ button[6].addEventListener("click", (event) => {
 button[7] = document.querySelector(`#row3 #column1`);    //2
 button[7].addEventListener("click", (event) => {
     if (disp.textContent.length < 11) {
-        disp.textContent = disp.textContent * 10 + +button[7].textContent;
-        num = disp.textContent;
+        result = result * 10 + +button[7].textContent;
+        disp.textContent = result;
+        number = result;
     } else {
         event.preventDefault();
     } 
@@ -119,8 +130,9 @@ button[7].addEventListener("click", (event) => {
 button[8] = document.querySelector(`#row3 #column2`);    //3
 button[8].addEventListener("click", (event) => {
     if (disp.textContent.length < 11) {
-        disp.textContent = disp.textContent * 10 + +button[8].textContent;
-        num = disp.textContent;
+        result = result * 10 + +button[8].textContent;
+        disp.textContent = result;
+        number = result;
     } else {
         event.preventDefault();
     } 
@@ -129,8 +141,9 @@ button[8].addEventListener("click", (event) => {
 button[9] = document.querySelector(`#row4 #column0`);    //0
 button[9].addEventListener("click", (event) => {
     if (disp.textContent.length < 11) {
-        disp.textContent = disp.textContent * 10 + +button[9].textContent;
-        num = disp.textContent;
+        result = result * 10 + +button[9].textContent;
+        disp.textContent = result;
+        number = result;
     } else {
         event.preventDefault();
     } 
@@ -138,85 +151,104 @@ button[9].addEventListener("click", (event) => {
 
 button[10] = document.querySelector(`#row3 #column3`);    // +
 button[10].addEventListener("click", () => {
-    operator = button[10].textContent;
-    number1 = +num;
-    clear();
+    operator[op] = button[10].textContent;   
+    operate(number, operator[op-1]);
+    op++;
+    number = sum;
+     
 });
 
 button[11] = document.querySelector(`#row2 #column3`);    // -
 button[11].addEventListener("click", () => {
-    operator = button[11].textContent;
-    number1 = +num;
-    clear();
+    operator[op] = button[11].textContent;    
+    operate(number, operator[op-1]);
+    op++;
+    number = sum;
+    
 });
 
 button[12] = document.querySelector(`#row1 #column3`);    // *
 button[12].addEventListener("click", () => {
-    operator = button[12].textContent;
-    number1 = +num;
-    clear();
+    operator[op] = button[12].textContent;
+    operate(number, operator[op-1]);    
+    op++;
+    number = sum;
     
 });
 
 button[13] = document.querySelector(`#row0 #column3`);    // /
 button[13].addEventListener("click", () => {
-    operator = button[13].textContent;
-    number1 = +num;
-    clear();
+    operator[op] = button[13].textContent;
+    operate(number, operator[op-1]);
+    op++;
+    number = sum;
     
 });
 
 button[14] = document.querySelector(`#row4 #column3`);    // =
 button[14].addEventListener("click", () => {
-    number2 = +num;
-    operate(number1, number2, operator);
-    disp.textContent = result;
+    operate(number, operator[op-1]);
+    op++;
+    number = sum;
 });
 
-button[15] = document.querySelector(`#row0 #column0`);
+button[15] = document.querySelector(`#row0 #column0`);    // AC
 button[15].addEventListener("click", () => {
     clear();
+    sum = 0;
+    op = 1;
+    operator = [];
+    operator[0] = "+";
+    number = 0;
 });
      
 
-function add(a, b) {
-    result = a + b;
-    return result;
+function add(a) {
+    sum = sum + a;
+    return sum;
 }
 
-function subtract(a, b) {
-    result = a - b;
-    return result;
+function subtract(a) {
+    sum = sum - a;
+    return sum;
 }
 
-function multiply(a, b) {
-    result = a * b;
-    return result;
+function multiply(a) {
+    sum = sum * a;
+    return sum;
 }
 
-function divide(a, b) {
-    result = a / b;
-    return result;
+function divide(a) {
+    sum = sum / a;
+    return sum;
 }
 
-function operate(number1, number2, operator) {
+function operate(number, operator) {
     switch(operator) {
         case '+':
-            add(number1, number2);
+            add(number);
+            clear();
+            disp.textContent = sum;
             break;
         case '-':
-            subtract(number1, number2);
+            subtract(number);
+            clear();
+            disp.textContent = sum;
             break;
         case '*':
-            multiply(number1, number2);
+            multiply(number);
+            clear();
+            disp.textContent = sum;
             break;
         case '/':
-            divide(number1, number2);
+            divide(number);
+            clear();
+            disp.textContent = sum;
             break;            
     }
 }
 
 function clear() {
-    num = 0;
     disp.textContent = 0;
+    result = 0;
 }
